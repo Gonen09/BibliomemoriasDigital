7a3da0e9d5e6f82ae5e1d1873e6ebc0641597a92
Gonzalo Santander <gonen.rt@gmail.com>
2016-11-15 12:07:31 -0300

    Agregado busqueda.css

--- busqueda.php
+++ busqueda.php
@@ -15,22 +15,8 @@
 
       <!-- Custom CSS -->
       <link href="css/general.css" rel="stylesheet">
-
-  <style>
-    /* Set height of the grid so .sidenav can be 100% (adjust as needed) */
-    .row.content {height: 550px}
-
-    /* Set gray background color and 100% height */
-    .sidenav {
-      background-color: #f1f1f1;
-      height: 100%;
-    }
-
-    /* On small screens, set height to 'auto' for the grid */
-    @media screen and (max-width: 767px) {
-      .row.content {height: auto;}
-    }
-  </style>
+      <link href="css/busqueda.css" rel="stylesheet">
+      
 </head>
 <body>
 
