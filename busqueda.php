59ace91b9c7f4431b04cefda68dd214438ec67d3
Gonzalo Santander <gonen.rt@gmail.com>
2016-11-23 21:32:29 -0300

    estructurado header,navbar y footer

--- busqueda.php
+++ busqueda.php
@@ -31,35 +31,38 @@
   </header> <!-- Header -->
 
   <!-- Navigation -->
-  <nav id="custom-bootstrap-menu" class="navbar navbar-default navbar-static-top" role="navigation">
-      <div class="container-fluid">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand">BiblioMemorias Digital</a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                  <li><a href="index.php">Inicio</a></li>
-                  <li class="active"><a href="#">Buscar Memorias</a></li>
-                  <li data-toggle="modal" data-target="#modal-contact"><a href="#">Formulario contacto</a></li>
-                  <li data-toggle="modal" data-target="#myModal1"><a href="#">Institución</a></li>
-                  <li data-toggle="modal" data-target="#myModal2"><a href="#">Docente</a></li>
-                  <li data-toggle="modal" data-target="#myModal3"><a href="#">Sobre nosotros</a></li>
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-                  <li data-toggle="modal" data-target="#modal-login"><a href="#"><span class="glyphicon glyphicon-cog"></span> Administrar</a></li>
-                </ul>
-            </div> <!-- /.navbar-collapse -->
-      </div> <!-- /.container -->
-    </nav>
-
+  <div class="row">
+    <div class="col-md-12">
+      <nav id="custom-bootstrap-menu" class="navbar navbar-default navbar-static-top" role="navigation">
+          <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand">BiblioMemorias Digital</a>
+                </div>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                      <li><a href="index.php">Inicio</a></li>
+                      <li class="active"><a href="#">Buscar Memorias</a></li>
+                      <li data-toggle="modal" data-target="#modal-contact"><a href="#">Formulario contacto</a></li>
+                      <li data-toggle="modal" data-target="#myModal1"><a href="#">Institución</a></li>
+                      <li data-toggle="modal" data-target="#myModal2"><a href="#">Docente</a></li>
+                      <li data-toggle="modal" data-target="#myModal3"><a href="#">Sobre nosotros</a></li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                      <li data-toggle="modal" data-target="#modal-login"><a href="#"><span class="glyphicon glyphicon-cog"></span> Administrar</a></li>
+                    </ul>
+                </div> <!-- /.navbar-collapse -->
+          </div> <!-- /.container -->
+        </nav>
+    </div>
+  </div>
 
     <!-- Modal Contact -->
     <div class="modal fade modal-ext" data-keyboard="false" data-backdrop="static" id="modal-contact" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
--- index.php
+++ index.php
@@ -22,43 +22,46 @@
 
   <!-- Header -->
   <header>
-      <div class="row">
-        <div align="center" class="col-lg-12">
-              <img src="image/principal/banner.png" class="img-responsive" id="banner">
-        </div> <!-- /.col-lg-12 -->
-      </div>  <!-- /.row -->
+    <div class="row">
+      <div align="center" class="col-lg-12">
+            <img src="image/principal/banner.png" class="img-responsive" id="banner">
+      </div> <!-- /.col-lg-12 -->
+    </div>  <!-- /.row -->
   </header> <!-- Header -->
 
   <!-- Navigation -->
-  <nav id="custom-bootstrap-menu" class="navbar navbar-default navbar-static-top" role="navigation">
-      <div class="container-fluid">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand">BiblioMemorias Digital</a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                  <li class="active"><a href="index.php">Inicio</a></li>
-                  <li><a href="busqueda.php">Buscar Memorias</a></li>
-                  <li data-toggle="modal" data-target="#modal-contact"><a href="#">Formulario contacto</a></li>
-                  <li data-toggle="modal" data-target="#myModal1"><a href="#">Institución</a></li>
-                  <li data-toggle="modal" data-target="#myModal2"><a href="#">Docente</a></li>
-                  <li data-toggle="modal" data-target="#myModal3"><a href="#">Sobre nosotros</a></li>
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-                  <li data-toggle="modal" data-target="#modal-login"><a href="#"><span class="glyphicon glyphicon-cog"></span> Administrar</a></li>
-                </ul>
-            </div> <!-- /.navbar-collapse -->
-      </div> <!-- /.container -->
-    </nav>
-
+  <div class="row">
+    <div class="col-lg-12">
+      <nav id="custom-bootstrap-menu" class="navbar navbar-default navbar-static-top" role="navigation">
+          <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand">BiblioMemorias Digital</a>
+                </div>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                      <li class="active"><a href="index.php">Inicio</a></li>
+                      <li><a href="busqueda.php">Buscar Memorias</a></li>
+                      <li data-toggle="modal" data-target="#modal-contact"><a href="#">Formulario contacto</a></li>
+                      <li data-toggle="modal" data-target="#myModal1"><a href="#">Institución</a></li>
+                      <li data-toggle="modal" data-target="#myModal2"><a href="#">Docente</a></li>
+                      <li data-toggle="modal" data-target="#myModal3"><a href="#">Sobre nosotros</a></li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                      <li data-toggle="modal" data-target="#modal-login"><a href="#"><span class="glyphicon glyphicon-cog"></span> Administrar</a></li>
+                    </ul>
+                </div> <!-- /.navbar-collapse -->
+          </div> <!-- /.container -->
+        </nav>
+    </div>
+  </div>
 
     <!-- Modal Contact -->
     <div class="modal fade modal-ext" data-keyboard="false" data-backdrop="static" id="modal-contact" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
@@ -345,10 +348,8 @@
         <div class="row">
           <div align="center" class="col-lg-12">
                 <img src="image/principal/footer_uls.png" id="foot" class="img-responsive"/>
-            </div>
-            <!-- /.col-lg-12 -->
-        </div>
-        <!-- /.row -->
+          </div> <!-- /.col-lg-12 -->
+        </div> <!-- /.row -->
     </footer>
 
     <!-- jQuery -->
