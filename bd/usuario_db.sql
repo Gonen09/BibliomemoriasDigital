94135282b45babedc0d843b9c4026188614f139c
Gonzalo Santander <gonen.rt@gmail.com>
2016-12-01 23:34:53 -0300

    modificados script poblar, conectar y testear BD

--- bd/usuario_db.sql
+++
@@ -1,2 +0,0 @@
-CREATE USER 'memoteca' IDENTIFIED BY '1234';
-GRANT SELECT ON memoteca.administradores TO memoteca;
---
+++ pagina.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <?php
+      require('php/conectar.php');
+     ?>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Pagina prueba bd</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+  </head>
+  <body>
+    <h1>Pagina prueba conexión</h1>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  </body>
+</html>
