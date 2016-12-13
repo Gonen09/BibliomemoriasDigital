41ec8c3117788fe27d45aef838617ca2b66d5a48
Gonzalo Santander <gonen.rt@gmail.com>
2016-12-13 17:26:59 -0300

    conectado modal contacto con base de datos memoteca

---
+++ bd/permisos_contacto.sql
@@ -0,0 +1,2 @@
+GRANT SELECT ON memoteca.contactos to 'memorias'@'localhost';
+GRANT INSERT ON memoteca.contactos to 'memorias'@'localhost';
--- php/contacto.php
+++ php/contacto.php
@@ -4,24 +4,19 @@
 	include('conectar.php');
 
 	date_default_timezone_set('Chile/Continental');
-	echo
 
 	$nombre=$_POST["nombre"];
 	$mail=$_POST["correo"];
 	$telefono=$_POST["telefono"];
 	$motivo=$_POST["motivo"];
 	$comentario=$_POST["comentario"];
-	//getdate();
 
-	$fecha = date("d-m-Y H:i");
-
-	$stmt = $conn->prepare("INSERT INTO contactos VALUES(null,:nombre,:mail,:telefono,:motivo,:comentario,:fecha,0);");
+	$stmt = $conn->prepare("INSERT INTO contactos VALUES(null,:nombre,:mail,:telefono,:motivo,:comentario,NOW(),0);");
 	$stmt->bindParam(':nombre',$nombre);
 	$stmt->bindParam(':mail',$mail);
 	$stmt->bindParam(':telefono',$telefono);
 	$stmt->bindParam(':motivo',$motivo);
 	$stmt->bindParam(':comentario',$comentario);
-	$stmt->bindParam(':fecha',$fecha);
 
 	if($stmt->execute()){
 		print ("<script>alert('Comentario enviado con éxito.');</script>");
@@ -29,7 +24,6 @@
 		print ("<script>alert('Hemos tenido un error al ingresar su comentario, por favor inténtelo de nuevo.');</script>");
 	}
 
-	$conn->close();
 	print ("<script>window.history.back();</script>");
 	exit();
 ?>
