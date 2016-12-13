95e6b903e44a610152ae5ae84bdbf0ca7fb4d851
Gonzalo Santander <gonen.rt@gmail.com>
2016-12-13 11:44:20 -0300

    dependencias necesarias backend contacto

---
+++ php/contacto.php
@@ -0,0 +1,21 @@
+<?php
+	$nombre=$_POST["nombre"];
+	$mail=$_POST["correo"];
+	$telefono=$_POST["telefono"];
+	$motivo=$_POST["motivo"]; //Rotular name y values en el formulario del html.
+	$comentario=$_POST["comentario"]; //rotular el name en el formulario del html.
+	$fecha=getdate();
+	
+	include("conexiones/admin_users.php");
+	
+	$stmt = $conn->prepare("INSERT INTO contactos VALUES(null,:nombre,:mail,:telefono,:motivo,:comentario,:fecha,0);");
+	$stmt->bindParam(':nombre',$nombre);
+	$stmt->bindParam(':mail',$mail);
+	$stmt->bindParam(':telefono',$telefono);
+	$stmt->bindParam(':motivo',$motivo);
+	$stmt->bindParam(':comentario',$comentario);
+	$stmt->bindParam(':fecha',$fecha);
+	if($stmt->execute()) echo "Comentario enviado con éxito.";
+	else echo "Hemos tenido un error al ingresar su comentario, por favor inténtelo de nuevo."
+
+?>
