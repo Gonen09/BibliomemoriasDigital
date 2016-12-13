404b03bb68afe5c3bd9ce28cf57012a3f2db7c7f
Gonzalo Santander <gonen.rt@gmail.com>
2016-12-13 12:44:24 -0300

    encapsulado ventana modal contacto

--- php/contacto.php
+++ php/contacto.php
@@ -1,13 +1,15 @@
 <?php
+
+	header('Content-Type: text/html; charset=UTF-8');
+	include('conectar.php');
+
 	$nombre=$_POST["nombre"];
 	$mail=$_POST["correo"];
 	$telefono=$_POST["telefono"];
 	$motivo=$_POST["motivo"]; //Rotular name y values en el formulario del html.
 	$comentario=$_POST["comentario"]; //rotular el name en el formulario del html.
 	$fecha=getdate();
-	
-	include("conexiones/admin_users.php");
-	
+
 	$stmt = $conn->prepare("INSERT INTO contactos VALUES(null,:nombre,:mail,:telefono,:motivo,:comentario,:fecha,0);");
 	$stmt->bindParam(':nombre',$nombre);
 	$stmt->bindParam(':mail',$mail);
@@ -15,7 +17,11 @@
 	$stmt->bindParam(':motivo',$motivo);
 	$stmt->bindParam(':comentario',$comentario);
 	$stmt->bindParam(':fecha',$fecha);
-	if($stmt->execute()) echo "Comentario enviado con éxito.";
-	else echo "Hemos tenido un error al ingresar su comentario, por favor inténtelo de nuevo."
+
+	if($stmt->execute()){
+		echo "Comentario enviado con éxito.";
+	}else{
+		echo "Hemos tenido un error al ingresar su comentario, por favor inténtelo de nuevo."
+	}
 
 ?>
