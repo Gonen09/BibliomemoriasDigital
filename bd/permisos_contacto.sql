30af9302301bd3e475bd40340fb3d0ef4a7e60ab
Gonzalo Santander <gonen.rt@gmail.com>
2016-12-15 03:44:31 -0300

    actualizados permisos tabla contactos

--- bd/permisos_contacto.sql
+++ bd/permisos_contacto.sql
@@ -1,2 +1,4 @@
 GRANT SELECT ON memoteca.contactos to 'memorias'@'localhost';
 GRANT INSERT ON memoteca.contactos to 'memorias'@'localhost';
+GRANT UPDATE ON memoteca.contactos to 'memorias'@'localhost';
+GRANT DELETE ON memoteca.contactos to 'memorias'@'localhost';
